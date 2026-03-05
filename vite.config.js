1a07c108743f931ed4b80527e06e8e0ef1bb1ca6
utsavsinghthc-cmd <utsavsinghthc@gmail.com>
2026-03-05 19:13:45 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/projectile-simulator/', // CHANGE THIS if your repo name is different
+})
